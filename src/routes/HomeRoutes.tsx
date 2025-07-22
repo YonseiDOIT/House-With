0e42dceb00c2a11bec27e2f0843cdeb3bdecf2c3
kwakmincheol123 <arnold2246@gmail.com>
2025-07-22 21:07:54 +0900

    [feat] 합치기

---
+++ src/routes/HomeRoutes.tsx
@@ -0,0 +1,9 @@
+import { Route } from 'react-router-dom';
+import Home from '../pages/Home/Home';
+
+export const HomeRoutes = (
+  <>
+    <Route path="/home-home" element={<Home />} />
+
+  </>
+);
